b4ace79d2e3a146c492c53f30881f53f277a742d
A-Aboo <a.abozer.n@gmail.com>
2026-09-18 10:07:17 +0200

    last question

--- TME01/Ex3.ml
+++ TME01/Ex3.ml
@@ -22,3 +22,14 @@ let duet_adder ( (a1 , a2) :  bool * bool  )  (  (b1 , b2) : bool * bool )  (c1 
     let (s1 , r1 ) = adder a1 b1 r2 in 
     (  (s1 , s2 ), r1)
 
+let quartet_adder ((a1,a2,a3,a4):bool*bool*bool*bool) ((b1,b2,b3,b4):bool*bool*bool*bool) (c:bool) : (bool*bool*bool*bool)* bool=
+    let ((s3 , s4), carry1 )= duet_adder (a3,a4) (b3,b4) c in
+    let ((s1 , s2) , carry2)= duet_adder (a1,a2) (b1,b2) carry1 in
+    ((s1 , s2 , s3 , s4 ) , carry2);;
+
+let to_quartet (n : int) : bool * bool * bool * bool = 
+    let b1 = (n/8) mod 2 = 1 in 
+    let b2 = (n/4) mod 2 = 1 in 
+    let b3 = (n/4) mod 2 = 1 in 
+    let b4 = n mod 2 = 1 in 
+    (b1,b2,b3,b4);;
