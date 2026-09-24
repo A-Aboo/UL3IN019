ebc1fe7c3755b3c48cc4f6ef1807f0446738d127
A-Aboo <a.abozer.n@gmail.com>
2026-09-24 22:19:03 +0200

    TME2 Exo1

---
+++ TME/TME02/Ex1.ml
@@ -0,0 +1,34 @@
+let rec lacunaire (l1 : 'a list) ( l2 : 'a list) : bool =
+  match l1 with 
+  | [] -> true 
+  | h1 :: t1 -> 
+    match l2 with 
+    | [] -> false 
+    | h2 :: t2 -> 
+      if h1 = h2 then 
+        lacunaire t1 t2
+      else 
+        lacunaire (h1::t1) (t2)
+
+let rec soud_adj (l1 : 'a list ) ( l2 : 'a list) (n : 'a)  : bool =
+    match l1 , l2 with
+    | [] , []  -> true
+    | [] , _ -> false
+    | _ , [] -> false
+    | h1::t1 , h2 :: t2 ->
+      if h1 = n then 
+        soud_adj t1 t2 n
+      else if h1 = h2 then soud_adj t1 t2 n
+       else false
+    
+let rec etendre (l1 : 'a list) (l2 : 'a list) (n : 'a) : 'a list =
+  match l1 , l2 with 
+  | [] , [] -> []
+  | [] , _ :: t2 -> 
+      n :: etendre [] t2 n
+  | _::_ , [] -> failwith "Errororor"
+  | h1 :: t1 , h2 :: t2 ->
+    if h1 = h2 then 
+      h1 :: etendre t1 t2 n
+    else 
+      n :: etendre l1 t2 n
