8b02e5a5c9d40cd3c17952d31a4028d0bfa1f305
A-Aboo <a.abozer.n@gmail.com>
2026-09-25 15:01:09 +0200

    premier exo termine

--- TME/TME02/Ex1.ml
+++ TME/TME02/Ex1.ml
@@ -10,15 +10,15 @@ let rec lacunaire (l1 : 'a list) ( l2 : 'a list) : bool =
       else 
         lacunaire (h1::t1) (t2)
 
-let rec soud_adj (l1 : 'a list ) ( l2 : 'a list) (n : 'a)  : bool =
+let rec sous_adj (l1 : 'a list ) ( l2 : 'a list) (n : 'a)  : bool =
     match l1 , l2 with
     | [] , []  -> true
     | [] , _ -> false
     | _ , [] -> false
     | h1::t1 , h2 :: t2 ->
       if h1 = n then 
-        soud_adj t1 t2 n
-      else if h1 = h2 then soud_adj t1 t2 n
+        sous_adj t1 t2 n
+      else if h1 = h2 then sous_adj t1 t2 n
        else false
     
 let rec etendre (l1 : 'a list) (l2 : 'a list) (n : 'a) : 'a list =
@@ -32,3 +32,26 @@ let rec etendre (l1 : 'a list) (l2 : 'a list) (n : 'a) : 'a list =
       h1 :: etendre t1 t2 n
     else 
       n :: etendre l1 t2 n
+
+
+let rec helper (l1 : 'a list) ( l2 : 'a list) (n : 'a) : 'a list =
+  match l1 , l2 with 
+  | [], [] -> []
+  | [] , _::_ -> failwith "Errorrrer"
+  | _::_ , [] -> failwith "Errorrere"
+  | h1 :: t1 , h2 :: t2 -> 
+    if h1 = h2 then 
+      h1 :: helper t1 t2 n
+    else if h1 = n then 
+        h2 :: helper t1 t2 n
+    else if h2 = n then   
+        h1 :: helper t1 t2 n
+    else 
+      failwith "unmatched numbers "
+
+let rec fusion_lists (lxt : 'a list list ) (n : 'a) : 'a list =
+  match lxt with 
+  | [] -> []
+  | [_] -> List.hd lxt 
+  | h1 ::  t -> 
+    helper h1 (fusion_lists t n  ) n 
